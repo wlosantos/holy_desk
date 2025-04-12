6e1c153f7afd60d26f5bdc9524eb14ef839f9b6e
wendellopes <wendelopes@msn.com>
2025-04-12 17:25:27 -0300

    setting letter-opener and skip authenticate index

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,8 +5,8 @@ FactoryBot.define do
     password_confirmation { password }
     # adicionar os campos do confirmable
     confirmed_at { Time.current }
-    confirmation_token { nil }
-    confirmation_sent_at { nil }
+    confirmation_token { SecureRandom.uuid }
+    confirmation_sent_at { Time.current }
     unconfirmed_email { nil } # Only if using reconfirmable
     # adicionar os campos do trackable
     sign_in_count { 0 }
@@ -19,15 +19,12 @@ FactoryBot.define do
       confirmed_at { nil }
       confirmation_token { SecureRandom.hex(10) }
       confirmation_sent_at { Time.current }
-      unconfirmed_email { email }
     end
 
     # adicionar uma trait para a confirmação de email
     trait :confirmed do
       confirmed_at { Time.current }
-      confirmation_token { SecureRandom.hex(10) }
-      confirmation_sent_at { nil }
-      unconfirmed_email { nil }
+      confirmation_sent_at { Time.current }
     end
     # adicionar uma trait para o trackable
     trait :trackable do
