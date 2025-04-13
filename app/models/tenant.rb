827804d50c9ab59c7cf60b4d1f0c1d91a4743670
wendellopes <wendelopes@msn.com>
2025-04-13 08:27:43 -0300

    change use only id reference

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,3 +1,27 @@
 class Tenant < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+
+  # after_create_commit -> { broadcast_tenant_count }
+  # after_destroy_commit -> { broadcast_tenant_count }
+  after_commit :broadcast_tenant_count, on: [ :create, :destroy ]
+
+  private
+
+  # usando partial
+  # def broadcast_tenant_count
+  #   Turbo::StreamsChannel.broadcast_replace_to(
+  #     "tenant_count_channel",
+  #     target: "tenant-count",
+  #     partial: "tenants/tenant_count",
+  #     locals: { count: Tenant.count }
+  #   )
+  # end
+
+  def broadcast_tenant_count
+    Turbo::StreamsChannel.broadcast_update_to(
+      "tenant_count_channel",
+      target: "tenant-count",
+      html: Tenant.count.to_s
+    )
+  end
 end
